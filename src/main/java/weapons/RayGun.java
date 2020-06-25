a60874f06724fe9f9ae9e5c26461e44558974fc6
cjscrimmy <craigscrimgeour@yahoo.com>
2020-06-25 11:33:39 +0100

    ray gun class tested

--- src/main/java/weapons/RayGun.java
+++ src/main/java/weapons/RayGun.java
@@ -1,4 +1,19 @@
 package weapons;
 
-public class RayGun {
+import behaviours.IWeapon;
+
+public class RayGun implements IWeapon{
+    private String colour;
+
+    public RayGun(String colour){
+        this.colour = colour;
+    }
+
+    public String getColour(){
+        return this.colour;
+    }
+
+    public int attack(){
+        return 35;
+    }
 }
--- src/test/java/weaponTests/RayGunTest.java
+++ src/test/java/weaponTests/RayGunTest.java
@@ -1,4 +1,26 @@
 package weaponTests;
 
+import org.junit.Before;
+import org.junit.Test;
+import weapons.RayGun;
+
+import static org.junit.Assert.assertEquals;
+
 public class RayGunTest {
+    private RayGun rayGun;
+
+    @Before
+    public void before(){
+        rayGun = new RayGun("red");
+    }
+
+    @Test
+    public void hasColour(){
+        assertEquals("red", rayGun.getColour());
+    }
+
+    @Test
+    public void canAttack(){
+        assertEquals(35, rayGun.attack());
+    }
 }
